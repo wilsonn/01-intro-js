4725ddf8706cce5fe1b0aaa2eb2670cc7da812ee
wilson hernande neira mija <wilsonnm22@hotmail.com>
2024-01-29 06:48:03 -0500

    chore: exercices 11, 12, 13

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-const users = [
-  'pepe', 'ana', 'maria', 'juan', 'sebastian', 'carlos', 'josefa'
-];
+import { invoiceFindById } from "./data/invoices"
 
-//const [pepe, ana, maria, , , carlos] = users;
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const result = invoiceFindById(4);
+    if (result) {
+      resolve(result);
+    } else {
+      reject("Error: no existe la factura por el id.")
+    }
+  }, 2500);
+});
 
-const [pepe, ana, maria, ...rest] = users;
-console.log(pepe, ana, maria, ...rest);
-console.log(pepe, ana, maria, rest);
+promise.then((result) => {
+  console.log(result);
+  console.log("realizada alguna tarea con delay")
+}).catch((error) => {
+  console.error(error);
+});
